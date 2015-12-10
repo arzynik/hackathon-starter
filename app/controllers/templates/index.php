956afee0f2bccd909d17d0b4ee3beab6e4446e6a
Devin Smith <github@arzynik.com>
2015-12-10 10:00:01 -0800

    fixed last strict warning

--- app/controllers/templates/index.php
+++ app/controllers/templates/index.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 class Templates extends \Tipsy\Controller {
-	public function init() {
+	public function init($args = null) {
 		$this->inject(function($Request, $View) {
 			$View->config(['layout' => 'templates/template']);
 			$View->display('templates/'.preg_replace('/\.html$/', '', $Request->loc(1)));
