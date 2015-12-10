184c11e72ec02aab0a81c28b3174f0f0df51a48c
Devin Smith <github@arzynik.com>
2015-12-10 09:56:28 -0800

    removed demo pass

--- app/tipsy.php
+++ app/tipsy.php
@@ -5,8 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Tipsy\Tipsy;
 
 Tipsy::config('../config/*');
-//Tipsy::config(['db' => ['url' => 'mongodb://heroku_c28b9nv8:i2j6pvgoh8b9kgqil1norb427i@ds063124.mongolab.com:63124/heroku_c28b9nv8']]);
-//Tipsy::config(['db' => ['url' => 'mongodb://127.0.0.1']]);
+
 /*
 if (getenv('CLEARDB_DATABASE_URL')) {
 	$tipsy->config(['db' => ['url' => getenv('CLEARDB_DATABASE_URL')]]);
