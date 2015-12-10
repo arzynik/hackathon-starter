6f872df60f6765188a591b5bc40b9d4e05517773
Devin Smith <github@arzynik.com>
2015-12-09 22:32:43 -0800

    some styling changes

--- public/assets/app.js
+++ public/assets/app.js
@@ -48,81 +48,32 @@ angular.module('App', ['ngRoute', 'ngResource'])
 		$rootScope.title = function(title) {
 			document.title = title ? title + ' | ' + 'Hackathon Starter' : 'Hackathon Starter';
 		};
+
+		var p = Trianglify({variance: '0.77', seed: 'tzlqe', x_colors: 'random', cell_size: 30});
+		$rootScope.triangles = p.svg({includeNamespace: true});
+		$rootScope.triangles = '<svg style="-webkit-filter: brightness(.5)" xmlns="http://www.w3.org/2000/svg" width="600" height="400">' + $rootScope.triangles.innerHTML + '</svg>';
 	})
 
-	.service('UploadService', function($resource, $routeParams, $location, $rootScope) {
-
+	.service('DemoService', function($resource, $routeParams, $location, $rootScope) {
 		var up = $resource('/upload', {}, {
 			'upload': { 'method': 'POST', params : { 'action' : 'upload' }}
 		});
-
-		var file = $resource('/get/:id', {id: '@id'});
-
-		this.upload = function(d) {
-			var max = 20000000;
-
-			if (d.data.length > max) {
-				$rootScope.$broadcast('upload-error', 'File too big');
-			} else {
-				up.upload({}, d, function(f) {
-					console.log(f);
-					if (f.uid) {
-						$rootScope.$broadcast('uploaded', f);
-					} else {
-						$rootScope.$broadcast('upload-error', f);
-					}
-				}, function() {
-					$rootScope.$broadcast('upload-error', '500');
-				});
-			}
-		}
-
-		this.get = function(id, callback) {
-			var f = file.get({id: id}, function() {
-				if (f.uid) {
-					callback(f);
-				} else {
-					$rootScope.$broadcast('file-error');
-				}
-			}, function() {
-				$rootScope.$broadcast('file-error');
-			});
-		}
-
-		this.uploadFile = function(file) {
-
-			if (file.type) {
-				var type = file.type.split('/');
-
-				if (type[0] != 'text' && type[0] != 'image') {
-					$rootScope.$broadcast('upload-error', 'Unsupported file type');
-					return;
-				}
-			}
-
-			var paste = {};
-			var self = this;
-
-			var fileReader = new FileReader();
-
-			fileReader.onloadend = function(e) {
-				paste.type = file.type;
-				paste.data = this.result;
-				self.upload(paste);
-			};
-
-			fileReader.onerror = function(e) {
-				$rootScope.$broadcast('upload-error', 'Could not read file');
-			};
-
-			fileReader.readAsDataURL(file);
-		}
 	})
 
 	.controller('Home', function ($rootScope) {
 		$rootScope.title();
+		$('.splash').css('background-image', 'url(\'data:image/svg+xml;utf8,'+$rootScope.triangles+'\')');
 	})
 
 	.controller('Contact', function ($rootScope) {
 		$rootScope.title('Contact');
 	});
+
+
+$(window).scroll(function() {
+	if ($(document).scrollTop() > 100) {
+		$('.navbar').addClass('shrink');
+	} else {
+		$('.navbar').removeClass('shrink');
+	}
+});
